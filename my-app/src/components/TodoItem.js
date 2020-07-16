28e3e2f1cc45fa51c2e463e345dd27e6e354b590
DuskoAtanasovski <atanasovski95@yahoo.com>
2020-07-16 02:55:09 +0200

    adding files

--- my-app/src/components/TodoItem.js
+++ my-app/src/components/TodoItem.js
@@ -32,6 +32,8 @@ export class TodoItem extends Component {
 
 TodoItem.propTypes = {
   todo: PropTypes.object.isRequired,
+  markComplete: PropTypes.func.isRequired,
+  delTodo: PropTypes.func.isRequired
 };
 
 const btnStyle = {
